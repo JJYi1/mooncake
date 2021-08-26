e56d06f0450547ca09318b036093731f805bad4c
JJYi1 <jjy.com@hotmail.com>
2021-08-27 00:42:02 +0200

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ from main import chechkmooncake
 scheduler = BlockingScheduler()
 
 # Schedules the job_function to be executed Monday through Friday at between 12-16 at specific times.
-scheduler.add_job(chechkmooncake, 'cron', day_of_week='mon-sun', hour='10-23', minute='5,15,25,35,45,55', start_date='2021-08-26 12:00:00', timezone=None)
+scheduler.add_job(chechkmooncake, 'cron', day_of_week='mon-sun', hour='7-23', minute='5,15,25,35,45,55', start_date='2021-08-26 12:00:00', timezone=None)
 
 # Start the scheduler
 scheduler.start()
