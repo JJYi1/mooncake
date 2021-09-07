0b29c11c673149cd9602f43821f12055d935e60a
JJY <jjy@gmx.de>
2021-09-07 11:38:58 +0200

    mooncake stock monitor for amazing oriental

--- main.py
+++ main.py
@@ -26,15 +26,16 @@ def chechkmooncake():
     #DefaultItemList = ['广州酒家 利口福 凤梨馅 500克 / Likofu Pineapple Mash 500g', '广州酒家 利口福 绿茶馅 500克 / Likofu Green Tea Mash 500g', '广州酒家 利口福 绿豆沙馅 500克 / Likofu Mung Bean Paste 500g', '广州酒家 利口福 芋蓉馅 500克 / Likofu Taro Paste 500g', '日威 七星伴月 810g / Riwei Seven Stars Mooncake Gift Box (5 Flavours) 810g', '日威 五仁月饼 720克 / Riwei Mooncake Mixed Nuts 720g', '日威 双黄白莲蓉月饼 720克 / Riwei Mooncake Pure Lotus Paste With Double Yolk 720g ', '日威 绿茶味双黄月饼 4件 720g / Riwei Mooncake Double Yolks Green Tea 720g', '日威 迷你月饼 合家团圆 540克 / Riwei Mini Mooncake Mix (3 Flavours) 540g', '美心冰皮 红豆冰味月饼 2x60g / Hong Kong MX Snowy Twin Pack Red Bean Iced Drink 2x60g', '香港美心冰皮二人世界 芒果脆脆月饼 2x60g / Hong Kong MX Snowy Twin Pack Mango Crunch (2x60g) 120g', '香港美心冰皮二人世界 香甜芒果 (2x60g) 120g / Hong Kong MX Snowy Twin Pack Mango', '香港美心芒果栗子冰皮月饼 120克 / Hong Kong MX Snowy Twin Pack Mango Chestnut', '香港美心迷你版月饼 (2x70g) 140g / Hong Kong MX Mini Assorted Mooncakes (2x70g) 140g']
     #DefaultItemList = ['广州酒家 利口福 凤梨馅 500克 / Likofu Pineapple Mash 500g', '广州酒家 利口福 绿茶馅 500克 / Likofu Green Tea Mash 500g', '广州酒家 利口福 绿豆沙馅 500克 / Likofu Mung Bean Paste 500g', '广州酒家 利口福 芋蓉馅 500克 / Likofu Taro Paste 500g', '日威 七星伴月 810g / Riwei Seven Stars Mooncake Gift Box (5 Flavours) 810g', '日威 双黄白莲蓉月饼 720克 / Riwei Mooncake Pure Lotus Paste With Double Yolk 720g ', '美心冰皮 红豆冰味月饼 2x60g / Hong Kong MX Snowy Twin Pack Red Bean Iced Drink 2x60g', '香港美心芒果栗子冰皮月饼 120克 / Hong Kong MX Snowy Twin Pack Mango Chestnut', '香港美心迷你版月饼 (2x70g) 140g / Hong Kong MX Mini Assorted Mooncakes (2x70g) 140g']
     DefaultItemList = ['广州酒家 利口福 凤梨馅 500克 / Likofu Pineapple Mash 500g', '广州酒家 利口福 绿茶馅 500克 / Likofu Green Tea Mash 500g', '广州酒家 利口福 绿豆沙馅 500克 / Likofu Mung Bean Paste 500g', '广州酒家 利口福 芋蓉馅 500克 / Likofu Taro Paste 500g', '日威 七星伴月 810g / Riwei Seven Stars Mooncake Gift Box (5 Flavours) 810g', '日威 双黄白莲蓉月饼 720克 / Riwei Mooncake Pure Lotus Paste With Double Yolk 720g ', '美心冰皮 红豆冰味月饼 2x60g / Hong Kong MX Snowy Twin Pack Red Bean Iced Drink 2x60g', '香港美心 丰年美月礼盒月饼 6x70克 / Hong Kong MX Harvest Moon Assorted Mooncake 6x70g', '香港美心精装八宝礼盒月饼 (8 Flavours) 675g / Hong Kong MX Mooncake Exclusive Selection (8 Flavours) 675g', '香港美心芒果栗子冰皮月饼 120克 / Hong Kong MX Snowy Twin Pack Mango Chestnut', '香港美心迷你版月饼 (2x70g) 140g / Hong Kong MX Mini Assorted Mooncakes (2x70g) 140g']
+    #DefaultItemList = ['广州酒家 利口福 凤梨馅 500克 / Likofu Pineapple Mash 500g', '广州酒家 利口福 绿茶馅 500克 / Likofu Green Tea Mash 500g', '广州酒家 利口福 绿豆沙馅 500克 / Likofu Mung Bean Paste 500g', '广州酒家 利口福 芋蓉馅 500克 / Likofu Taro Paste 500g', '日威 七星伴月 810g / Riwei Seven Stars Mooncake Gift Box (5 Flavours) 810g', '日威 双黄白莲蓉月饼 720克 / Riwei Mooncake Pure Lotus Paste With Double Yolk 720g ', '美心冰皮 红豆冰味月饼 2x60g / Hong Kong MX Snowy Twin Pack Red Bean Iced Drink 2x60g', '香港美心 丰年美月礼盒月饼 6x70克 / Hong Kong MX Harvest Moon Assorted Mooncake 6x70g', '香港美心精装八宝礼盒月饼 (8 Flavours) 675g / Hong Kong MX Mooncake Exclusive Selection (8 Flavours) 675g', '香港美心迷你版月饼 (2x70g) 140g / Hong Kong MX Mini Assorted Mooncakes (2x70g) 140g']
     curquantity = len(itemlist)
     print(f"\nNow they are providing {curquantity} kinds of mooncakes\n{datetime.now()}\n============================================")
     #pushmessageHNV(f"没有新月饼")
 
-    if curquantity != 11:
-        if curquantity < 11:
+    if curquantity != 10:
+        if curquantity < 10:
             changeditem = set(DefaultItemList) - set(itemlist)
             pushmessageHNV(f"有月饼下架啦, 下架的月饼有{changeditem}")
-        if curquantity > 11:
+        if curquantity > 10:
             changeditem = set(itemlist) - set(DefaultItemList)
             pushmessageHNV(f"有月饼上架啦, 新上的月饼有{changeditem}")
 
