7e14b210623aae6b970c7384a11a6d20c732bfc4
JJYi1 <jjy.com@hotmail.com>
2021-11-24 23:03:14 +0100

    Update push.py

--- push.py
+++ push.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 import paho.mqtt.client as mqtt
+username = 'your user name'
+pwd = 'your password'
+server = 'your server'
+channel = 'your channel'
 
 def pushmessageHNV(msg):
     client = mqtt.Client('pymooncake')
-    client.username_pw_set("jjy000", "qazwsxedc123")
-    client.connect('mqtt.arduino-hannover.de', port=1883)
-    client.publish('jjy000/mooncake', msg)
+    client.username_pw_set(username, pwd)
+    client.connect(server, port=1883)
+    client.publish(channel, msg)
 
 # client.connect('test.mosquitto.org', port=1883)
